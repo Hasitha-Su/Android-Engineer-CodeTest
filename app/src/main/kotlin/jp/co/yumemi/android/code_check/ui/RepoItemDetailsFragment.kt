d3fb7fd798eac8f1b34829f809570e7e0a539581
Hasitha-KMS <hasithakms@absi.digital>
2024-05-08 19:07:04 +0530

    Refactor RepoItemDetailsFragment and update associated XML layouts
    - Simplified RepoItemDetailsFragment to enhance readability and maintenance.
    - Updated XML layouts to improve UI consistency and remove redundant elements.
    - Added new styles for TextInputLayout to enhance the visual appeal and UX of text input fields.

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/RepoItemDetailsFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/RepoItemDetailsFragment.kt
@@ -1,27 +1,19 @@
 package jp.co.yumemi.android.code_check.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.navArgs
 import coil.load
 import dagger.hilt.android.AndroidEntryPoint
 import jp.co.yumemi.android.code_check.R
 import jp.co.yumemi.android.code_check.databinding.FragmentRepoItemDetailsBinding
 import jp.co.yumemi.android.code_check.view.RepoItemDetailsViewModel
-import jp.co.yumemi.android.code_check.view.RepoSearchViewModel
 @AndroidEntryPoint
 class RepoItemDetailsFragment : Fragment(R.layout.fragment_repo_item_details) {
 
     private var _binding: FragmentRepoItemDetailsBinding? = null
     private val binding get() = _binding!!
-
-    //private val viewModel: RepoSearchViewModel by activityViewModels()
-
     private val viewModel: RepoItemDetailsViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -34,11 +26,10 @@ class RepoItemDetailsFragment : Fragment(R.layout.fragment_repo_item_details) {
                     binding.apply {
                         ownerIconView.load(owner.avatarUrl)
                         nameView.text = name
-                        languageView.text = language
-                        starsView.text = getString(R.string.stars, stargazersCount)
-                        watchersView.text = getString(R.string.watchers, watchersCount)
-                        forksView.text = getString(R.string.forks, forksCount)
-                        openIssuesView.text = getString(R.string.open_issues, openIssuesCount)
+                        starsView.text = stargazersCount.toString()
+                        watchersView.text = watchersCount.toString()
+                        forksView.text = forksCount.toString()
+                        //textDescription.text = description
                     }
                 }
             }

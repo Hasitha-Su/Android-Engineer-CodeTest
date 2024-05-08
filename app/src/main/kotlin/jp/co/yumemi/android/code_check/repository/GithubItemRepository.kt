45ae8536336d29de8316d6616efd45f55035f629
Hasitha-KMS <hasithakms@absi.digital>
2024-05-08 17:01:09 +0530

    Enhance GithubItemRepository with dependency injection and coroutine support
    - Refactored GithubItemRepository to include dependency injection via constructor injection of GithubApiService.
    - Implemented coroutine support using Kotlin's withContext for network operations to ensure calls are made on an appropriate background thread (IO dispatcher).
    - Added error handling in getGithubRepositoryList to return null if the API response is unsuccessful, improving robustness.
    - Integrated changes ensure cleaner architecture by decoupling the repository from the ViewModel and facilitating easier unit testing.

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/GithubItemRepository.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/GithubItemRepository.kt
@@ -1,11 +1,29 @@
 package jp.co.yumemi.android.code_check.repository
 
+import jp.co.yumemi.android.code_check.model.GithubRepoSearchResponse
+import jp.co.yumemi.android.code_check.network.GithubApiService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 /**
  * Repository class responsible for fetching GitHub repository data.
  *
  * This class acts as an intermediary between the ViewModel and the GitHub API service.
  * It provides methods to fetch repository data from the remote data source using the provided API service.
  *
+ * @param itemApiService The service interface for communicating with the GitHub API.
+ *
  */
-class GithubItemRepository  {
+class GithubItemRepository @Inject constructor(
+    private val itemApiService: GithubApiService
+) {
+    suspend fun getGithubRepoInfo(query: String): GithubRepoSearchResponse? =
+        withContext(Dispatchers.IO) {
+            getGithubRepositoryList(query)
+        }
+
+    private suspend fun getGithubRepositoryList(query: String): GithubRepoSearchResponse? {
+        val response = itemApiService.getItems(query)
+        return if (response.isSuccessful) response.body() else null
+    }
 }
